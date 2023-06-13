b6bdba99480bb99c612a11cc6891dd06404e0895
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-06-13 23:25:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
+
+export default {
+  data() {
+    return {
+      baseUrl: "http://127.0.0.1:8000",
+      projects: []
+    };
+  },
+  mounted() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get(`${this.baseUrl}/api/projects`).then(resp => {
+        this.projects = resp.data.results;
+      });
+    }
+  },
+  components: { ProjectCard }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h2>Lista dei progetti</h2>
+    <div class="row row-cols-3 g-3">
+      <div class="col" v-for="project in projects" :key="project.id">
+        <ProjectCard :project="project" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    },
+    data() {
+        return {
+            baseUrl: "http://127.0.0.1:8000",
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <div class="card-body">
+            <h5>{{ project.title }}</h5>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
